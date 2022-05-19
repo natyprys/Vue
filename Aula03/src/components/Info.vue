0c28404747d807343fcaeccecd03a5e2f83f6b5a
natyprys <nathalia.p.lins@gmail.com>
2022-05-19 15:24:25 -0300

    diretivas

--- Aula03/src/components/Info.vue
+++ Aula03/src/components/Info.vue
@@ -1,17 +1,31 @@
 <template>
     <div>
-        <p>Estou trabalhando no momento.</p>
+        <!-- para diretivas if e else a condição deve ser colocar no data -->
+        <p v-if="esta_trabalhando" >Estou trabalhando no momento.</p>
+        <!-- o if e o else devem estar juntos -->
+        <p v-else> Estou em busca de novas oportunidades </p>
         <p>Utilizo as seguintes tecnologias:</p>
         <ul>
             <li>Java</li>
             <li>Angular</li>
             <li>Python</li>   
         </ul>
+        <p v-show="mostrar_email">Mande uma  mensagem para: {{ email }} </p>
+        <!-- mostra dependendo do mostrar-email -->
     </div>
 </template>
 
+
+
 <script>
 export default{
-    name:'Info'
+    name:'Info',
+    data(){
+        return{
+            esta_trabalhando: false,
+            mostrar_email: false,
+            email: 'nathalia.p.lins@gmail.com'
+        }
+    }
 }
 </script>
